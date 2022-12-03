af0e3142cb2597f026281549debe6dca2cfc3631
sharamall <sharamall@hotmail.com>
2022-12-03 01:03:52 -0500

    day3

---
+++ day3part1.rb
@@ -0,0 +1,31 @@
+require 'set'
+
+sum = 0
+file = File.open "day3in.txt"
+file.each_line do |l|
+  first_ = l[0...(l.length / 2)]
+  second_ = l[(l.length / 2)..l.length]
+  first_map = {}
+  second_map = {}
+
+  first_.each_char do |e|
+    first_map[e] = e
+  end
+
+  second_.each_char do |e|
+    second_map[e] = e
+  end
+
+  first_map.each do |k, v|
+    if second_map.has_key? k
+      puts "shared #{k}"
+      if k.ord < 97 # upper
+        sum += (k.ord) - 64 + 26
+      elsif
+        sum += (k.ord) - 96
+      end
+    end
+  end
+
+end
+puts sum
---
+++ day3part2.rb
@@ -0,0 +1,42 @@
+require 'set'
+
+sum = 0
+file = File.open "day3in.txt"
+i = 0
+map1 = {}
+map2 = {}
+map3 = {}
+sum = 0
+file.each_line do |l|
+  l.strip!
+  map = {}
+  l.each_char do |c|
+    map[c] = c
+  end
+
+  if i == 0
+    map1 = map
+  elsif i == 1
+    map2 = map
+  else
+    map3 = map
+  end
+
+  i += 1
+
+  if i == 3
+    map1.each do |k, v|
+      if map2.has_key? k
+        if map3.has_key? k
+          if k.ord < 97 # upper
+            sum += (k.ord) - 64 + 26
+          elsif
+            sum += (k.ord) - 96
+          end
+        end
+      end
+    end
+    i = 0
+  end
+end
+puts sum
